03198b52213f1aa9c5f4f5f214ec4cd6ccacece2
Daniel Harris <125743538+danh686@users.noreply.github.com>
2023-02-21 12:06:08 +0000

    Update Speed converter.py

--- Speed converter.py
+++ Speed converter.py
@@ -16,27 +16,31 @@ def clear():
     mph.config(text= "")
     kph.config(text= "")
 
-root.geometry("320x200")
-root.resizable(True, True)
-root.title("Speed converter")
-root.configure(bg= "Light Blue")
+def main():
+    root.geometry("320x200")
+    root.resizable(True, True)
+    root.title("Speed converter")
+    root.configure(bg= "Light Blue")
 
-frameEntry= Frame(root).grid(row= 0, column= 0, columnspan= 2, padx= 15, pady= 15)
+    frameEntry= Frame(root).grid(row= 0, column= 0, columnspan= 2, padx= 15, pady= 15)
 
-Label(frameEntry, text= "Kph").grid(column=0, row= 0, padx= 5, pady= 5)
-kphEntry= Entry(frameEntry, width=15, bg= "White").grid(column=0, row=1, padx= 5, pady= 5)
+    Label(frameEntry, text= "Kph").grid(column=0, row= 0, padx= 5, pady= 5)
+    kphEntry= Entry(frameEntry, width=15, bg= "White").grid(column=0, row=1, padx= 5, pady= 5)
 
-Label(frameEntry, text= "Mph").grid(column= 2, row= 0, padx= 15, pady= 5)
-mph= Label(frameEntry, width= 15, bg= "White").grid(column= 2, row= 1, padx= 5, pady= 5)
+    Label(frameEntry, text= "Mph").grid(column= 2, row= 0, padx= 15, pady= 5)
+    mph= Label(frameEntry, width= 15, bg= "White").grid(column= 2, row= 1, padx= 5, pady= 5)
 
-Label(frameEntry, text= "Mph").grid(column=0, row= 3, padx= 5, pady= 5)
-mphEntry= Entry(frameEntry,width= 15, bg= "White").grid(column= 0, row= 4, padx= 5, pady= 5)
+    Label(frameEntry, text= "Mph").grid(column=0, row= 3, padx= 5, pady= 5)
+    mphEntry= Entry(frameEntry,width= 15, bg= "White").grid(column= 0, row= 4, padx= 5, pady= 5)
 
-Label(frameEntry, text= "Kph").grid(column=2, row= 3, padx=5, pady=5)
-kph= Label(frameEntry, width= 15, bg= "White").grid(column= 2, row= 4, padx= 5, pady= 5)
+    Label(frameEntry, text= "Kph").grid(column=2, row= 3, padx=5, pady=5)
+    kph= Label(frameEntry, width= 15, bg= "White").grid(column= 2, row= 4, padx= 5, pady= 5)
 
-Button(frameEntry, text= "Convert", command= mphToKph).grid(column=1, row= 4, padx= 5, pady= 5)
-Button(frameEntry, text= "Convert", command= kphToMph).grid(column= 1, row= 1, padx= 15, pady= 5)
-Button(frameEntry, text= "Clear all", command= clear).grid(column=1, row= 5, padx= 5, pady= 15)
+    Button(frameEntry, text= "Convert", command= mphToKph).grid(column=1, row= 4, padx= 5, pady= 5)
+    Button(frameEntry, text= "Convert", command= kphToMph).grid(column= 1, row= 1, padx= 15, pady= 5)
+    Button(frameEntry, text= "Clear all", command= clear).grid(column=1, row= 5, padx= 5, pady= 15)
 
-root.mainloop()
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
